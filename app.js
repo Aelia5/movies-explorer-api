d56a740ead6600111adb8029e173f1384ae28732
Olga Liubimova <lov800@yandex.ru>
2023-12-06 10:51:40 +0300

    Добавить Helmet

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+require('dotenv').config();
+
 const { errors } = require('celebrate');
 
 const { PORT = 3000 } = process.env;
@@ -8,14 +12,23 @@ const { PORT = 3000 } = process.env;
 const app = express();
 const router = require('./routes/index');
 const { handleError } = require('./middlewares/handleError');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  limit: 100,
+  standardHeaders: 'draft-7',
+  legacyHeaders: false,
+});
 
 mongoose.connect('mongodb://localhost:27017/bitfilmsdb', { useNewUrlParser: true });
 
+app.use(helmet());
+
 app.use(bodyParser.json());
 
-const { requestLogger, errorLogger } = require('./middlewares/logger');
-
 app.use(requestLogger);
+app.use(limiter);
 app.use(router);
 
 app.use(errorLogger);
